55f55862d5493954fc1aa8e72d0c8cd8c09b4298
Ryan Carson <rcarson95@gmail.com>
2018-07-12 15:43:27 -0400

    fixed logic. working on formatting

--- strange-sequence.js
+++ strange-sequence.js
@@ -1,28 +1,57 @@
-const sequence = (element) => {
-	var elemUse = element;
-	var output = 0;
-	elemNum = 0;
+// const sequence = (element) => {
+// 	var elemUse = element;
+// 	var output = 0;
+// 	elemNum = 0;
 
+// 	while (output != element) {
+// 		console.log("a" + elemNum + "= ");
+// 		elemNum += 1;
+// 		let squareSum = 0;
+// 		for(var i = 0; i < elemUse.length; i++){
+// 			squareSum += charAt(i)^2;
+// 			if(i < elem.length -1){
+// 				console.log(charAt(i) + "^2 + ");
+// 			} else {
+// 				console.log(charAt(i) + "^2 = ");
+// 			}
+// 		}
+// 		console.log(squareSum);
+// 		if(squareSum != element){
+// 			elemUse == squareSum;
+// 		} else if(squareSum == element) {
+// 			output == element;
+// 		}
+// 	}
+// 	console.log(", which has already occured before (a0)")
+// }
+
+
+const sequence = (element) => {
+	var elemUse = element.toString();
+	var output = 0;
+	var elemNum = 0;
+
+	console.log("a0 = " + element)
 	while (output != element) {
-		console.log("a" + elemNum + "= ");
 		elemNum += 1;
+		process.stdout.write("a" + elemNum + "= ");
 		let squareSum = 0;
 		for(var i = 0; i < elemUse.length; i++){
-			squareSum += charAt(i)^2;
-			if(i < elem.length){
-				console.log(charAt(i) + "^2 + ");
+			squareSum += Math.pow(elemUse.charAt(i), 2);
+			if(i < elemUse.length -1){
+				process.stdout.write(elemUse.charAt(i) + "^2 + ");
 			} else {
-				console.log(charAt(i) + "^2 = ");
+				process.stdout.write(elemUse.charAt(i) + "^2 = ");
 			}
 		}
-		console.log(squareSum);
+		process.stdout.write(squareSum);
 		if(squareSum != element){
-			elemUse == squareSum;
+			elemUse = squareSum.toString();
 		} else if(squareSum == element) {
-			output == element;
+			output = element;
 		}
 	}
-	console.log(", which has already occured before (a0)")
-}
+	process.stdout.write(", which has already occured before (a0)");
+};
 
 console.log(sequence(16));
