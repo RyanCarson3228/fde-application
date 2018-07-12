9e68b8732da6496ff1c496569723b465c670a113
Ryan Carson <rcarson95@gmail.com>
2018-07-12 15:55:00 -0400

    Formatting fixed but now returns an undefined at the end

--- strange-sequence.js
+++ strange-sequence.js
@@ -1,37 +1,9 @@
-// const sequence = (element) => {
-// 	var elemUse = element;
-// 	var output = 0;
-// 	elemNum = 0;
-
-// 	while (output != element) {
-// 		console.log("a" + elemNum + "= ");
-// 		elemNum += 1;
-// 		let squareSum = 0;
-// 		for(var i = 0; i < elemUse.length; i++){
-// 			squareSum += charAt(i)^2;
-// 			if(i < elem.length -1){
-// 				console.log(charAt(i) + "^2 + ");
-// 			} else {
-// 				console.log(charAt(i) + "^2 = ");
-// 			}
-// 		}
-// 		console.log(squareSum);
-// 		if(squareSum != element){
-// 			elemUse == squareSum;
-// 		} else if(squareSum == element) {
-// 			output == element;
-// 		}
-// 	}
-// 	console.log(", which has already occured before (a0)")
-// }
-
-
 const sequence = (element) => {
 	var elemUse = element.toString();
 	var output = 0;
 	var elemNum = 0;
 
-	console.log("a0 = " + element)
+	console.log("a0 = " + element);
 	while (output != element) {
 		elemNum += 1;
 		process.stdout.write("a" + elemNum + "= ");
@@ -44,14 +16,15 @@ const sequence = (element) => {
 				process.stdout.write(elemUse.charAt(i) + "^2 = ");
 			}
 		}
-		process.stdout.write(squareSum);
+		console.log(squareSum);
 		if(squareSum != element){
 			elemUse = squareSum.toString();
 		} else if(squareSum == element) {
 			output = element;
+			console.log(squareSum + " has already occured before (a0)");
 		}
 	}
-	process.stdout.write(", which has already occured before (a0)");
+	
 };
 
 console.log(sequence(16));
